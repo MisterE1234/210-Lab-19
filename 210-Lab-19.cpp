c6103791fa9fca49140dffc4ee2a5425635d6337
MisterE1234 <MisterE1234@outlook.com>
2025-10-03 16:04:58 -0700

    2nd Commit

--- 210-Lab-19.cpp
+++ 210-Lab-19.cpp
@@ -1,2 +1,21 @@
 //COMSC-210|Lab-19|Ethan Aylard
 //IDE used: Visual Studio Code
+//This program uses a Movie class to display different movie review information by using linked lists and an external file.
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Review{
+double rating;
+string comment;
+Review *next;
+};
+
+class Movie{
+string title;
+Review *review;
+};
+
+
+
