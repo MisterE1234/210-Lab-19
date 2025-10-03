d2e4e8e79c98daa6e5732e36166b41d9391a30e6
MisterE1234 <MisterE1234@outlook.com>
2025-10-03 16:16:19 -0700

    3rd Commit

--- 210-Lab-19.cpp
+++ 210-Lab-19.cpp
@@ -13,9 +13,55 @@ Review *next;
 };
 
 class Movie{
+private:
 string title;
-Review *review;
+Review *head;
+
+public:
+
+//getters:
+string getTitle() const {return title;}
+Review* getReview() const {return review;}
+
+//setters:
+void setTitle(string t) {title = t;}
+
+//constructor:
+Movie(){
+    title = "No Title";
+    head = nullptr;
+}
+
+Movie(string t){
+    title = t;
+    review = nullptr;
+}
+
+Movie(string t, double r, string c){
+    title = t;
+    review = new Review;
+    review->rating = r;
+    review->comment = c;
+    review->next = nullptr;
+}
+
+//other methods:
+//adds a review to the linked list of reviews. If there are no reviews, the new review becomes the first review.
+void addReview(double r, string c){
+    Review *newReview = new Review;
+    newReview->rating = r;
+    newReview->comment = c;
+    newReview->next = nullptr;
+
+    if (review == nullptr){
+        review = newReview;
+    }
+    else{
+        Review *temp = ;
+        
+    }
+}
+
+
+
 };
-
-
-
