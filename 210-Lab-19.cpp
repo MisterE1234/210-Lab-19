6600d43062f14ae333b29efadf58462c523f5d22
MisterE1234 <MisterE1234@outlook.com>
2025-10-03 16:27:13 -0700

    4th Commit

--- 210-Lab-19.cpp
+++ 210-Lab-19.cpp
@@ -7,61 +7,87 @@
 using namespace std;
 
 struct Review{
-double rating;
-string comment;
-Review *next;
+    double rating;
+    string comment;
+    Review *next;
 };
 
 class Movie{
 private:
-string title;
-Review *head;
+    string title;
+    Review *head;
 
 public:
 
-//getters:
-string getTitle() const {return title;}
-Review* getReview() const {return review;}
+    //getters:
+    string getTitle() const {return title;}
+    Review* getReview() const {return head;}
 
-//setters:
-void setTitle(string t) {title = t;}
+    //setters:
+    void setTitle(string t) {title = t;}
 
-//constructor:
-Movie(){
-    title = "No Title";
-    head = nullptr;
-}
-
-Movie(string t){
-    title = t;
-    review = nullptr;
-}
-
-Movie(string t, double r, string c){
-    title = t;
-    review = new Review;
-    review->rating = r;
-    review->comment = c;
-    review->next = nullptr;
-}
-
-//other methods:
-//adds a review to the linked list of reviews. If there are no reviews, the new review becomes the first review.
-void addReview(double r, string c){
-    Review *newReview = new Review;
-    newReview->rating = r;
-    newReview->comment = c;
-    newReview->next = nullptr;
-
-    if (review == nullptr){
-        review = newReview;
+    //constructor:
+    Movie(){
+        title = "No Title";
+        head = nullptr;
     }
-    else{
-        Review *temp = ;
+
+    Movie(string t){
+        title = t;
+        head = nullptr;
+    }
+
+    Movie(string t, double r, string c){
+        title = t;
+        head = new Review;
+        head->rating = r;
+        head->comment = c;
+        head->next = nullptr;
+    }
+
+    //other methods:
+    //adds a review to the front of the linked list of reviews.
+    void addReview(double r, string c){
         
+        Review *newReview = new Review;
+        newReview->rating = r;
+        newReview->comment = c;
+        newReview->next = nullptr;
+
+        if (head == nullptr){
+            head = newReview;
+        }
+        else{
+            Review *temp = head;
+            newReview->next = temp;
+            head = newReview;
+        }
     }
-}
 
+    void printReviews() {
+        Review *temp = head;
+        int count = 0;
+     
+        if (temp == nullptr){
+            cout << "No reviews available." << endl;
+            return;
+        }
+        while (temp != nullptr){
+            count++;
 
-
+            cout << "Rating #" << count << ": " << temp->rating << endl;
+            cout << "Comment: " << temp->comment << endl << endl;        
+            temp = temp->next;
+        }
+    }
 };
+
+
+int main(){
+    int const size = 4;
+    ifstream inFile;
+    string movieTitle;
+    double movieRating;
+    string movieComment;
+    Movie movies[size];
+}
